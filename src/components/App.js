3a216e548dad597a9c407c21452d69f395a8517b
Kristian <kris.dziamka@gmail.com>
2022-10-25 21:04:58 +0300

    some

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import Header from "./Header/Header";
-import HomePage from "./HomePage/HomePage";
-import SearchFilm from "./SearchFilm/SearchFilm";
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Header/Header';
+import HomePage from './HomePage/HomePage';
+import SearchFilm from './SearchFilm/SearchFilm';
+import { Loading } from "notiflix/build/notiflix-loading-aio";
+
 
 export function App() {
-  
-
   return (
     <>
-    <Header/>
-  <SearchFilm/>
+
+<Suspense
+      fallback={Loading.circle({
+        svgColor: "#ff6b01",
+        cssAnimationDuration: 800,
+      })}
+    >
+      {Loading.remove()}
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route path = "movies" element={<SearchFilm/>}> </Route>
+      
+        </Route>
+      </Routes>
+      </Suspense>
+      {/* <SearchFilm/> */}
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import styles from "./Header.module.css"
 export default function Header() {
   return (
     <header className={styles.header}>
-        <h2 className={styles.title}>Home</h2>
-        <h2>Films</h2>
+        <a href="#" className={styles.title}>Home</a>
+        <a href="/movies">Films</a>
 
 
     </header>
